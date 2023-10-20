65e985b472e83e91d803465c21cd89696162c4a5
lostofan <kolog007@gmail.com>
2023-10-20 10:34:28 +0300

    chat layout

--- server/index.js
+++ server/index.js
@@ -6,15 +6,16 @@ const path = require('path');
 const { Server } = require('socket.io');
 const io = new Server(server);
 
+app.use(express.static(path.join(__dirname, '../client-js', 'public')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client-js', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client-js', 'index.html'));
 });
 
 io.on('connection', (socket) => {
   socket.on('login', (data) => {
     data ? (socket.username = data) : (socket.username = 'Аноним');
-
     io.emit('connected', socket.username);
+    io.emit('add user', socket.username);
   });
   socket.on('disconnect', () => {
     io.emit('leave', socket.username);
